e7c037d02069a7a7a5e93f2314410de78fa6fb02
Надюшка Рыбка <kitti-46@ya.ru>
2022-10-08 13:49:29 +0300

    added Task21HOME formating

--- Task21HOME/Program.cs
+++ Task21HOME/Program.cs
@@ -6,26 +6,28 @@ A (7,-5, 0); B (1,-1,9) -> 11.53*/
 
 Point GetPoint()
 {
-Point point = new Point();
-Console.WriteLine("Введите координату Х:");
-point.X = int.Parse(Console.ReadLine());
+    Point point = new Point();
+    Console.WriteLine("Введите координату Х:");
+    point.X = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите координату У:");
-point.Y = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите координату У:");
+    point.Y = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите координату Z:");
-point.Z = int.Parse(Console.ReadLine());
-return point;
+    Console.WriteLine("Введите координату Z:");
+    point.Z = int.Parse(Console.ReadLine());
+    return point;
 }
 
 Point firstPoint = GetPoint();
 Point secondPoint = GetPoint();
 
-double result = Math.Sqrt(Math.Pow(firstPoint.X - secondPoint.X, 2) + Math.Pow(firstPoint.Y - secondPoint.Y,2)+ Math.Pow(firstPoint.Z - secondPoint.Z,2));
+double result = Math.Sqrt(Math.Pow(firstPoint.X - secondPoint.X, 2)
++ Math.Pow(firstPoint.Y - secondPoint.Y, 2)
++ Math.Pow(firstPoint.Z - secondPoint.Z, 2));
 Console.WriteLine($"расстояние между двумя точками = {result}");
 class Point
 {
-public int X { get; set;}
-public int Y { get; set;}
-public int Z { get; set;}
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
 }
